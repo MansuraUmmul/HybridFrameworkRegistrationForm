73a943dd25b3847f818d47267567e8f79a9ea3a3
mansura <mansuraummulbashar@gmail.com>
2024-03-20 15:55:01 -0500

    de

--- src/main/java/ObjectRepository/HomePage.java
+++ src/main/java/ObjectRepository/HomePage.java
@@ -3,13 +3,13 @@ package ObjectRepository;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HomePage{
+public class HomePage extends BasePage{
 	WebDriver driver;
 	By sign_in=By.xpath("//a[normalize-space()='Sign In']");
 	
 	//Constructor
 	 public HomePage(WebDriver driver) {
-		 this.driver=driver;
+		 super(driver);
 	    }
 	
 	
